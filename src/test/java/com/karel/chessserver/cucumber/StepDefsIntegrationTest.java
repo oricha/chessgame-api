f72fd25d16360bd1417072ec7ab2c3e9a1fb59f5
Karel Muñiz <oricha@gmail.com>
2022-05-06 08:49:51 +0200

    New configuration for integration test

---
+++ src/test/java/com/karel/chessserver/cucumber/StepDefsIntegrationTest.java
@@ -0,0 +1,60 @@
+package com.karel.chessserver.cucumber;
+
+import io.cucumber.java.en.Given;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StepDefsIntegrationTest extends SpringIntegrationTest {
+
+    @Given("^a player start a game")
+    public void the_client_issues_GET_hello() throws Throwable {
+        executeGet("http://localhost:8080/start");
+    }
+
+    void executePost() throws IOException {
+        final Map<String, String> headers = new HashMap<>();
+        headers.put("Accept", "application/json");
+        final HeaderSettingRequestCallback requestCallback = new HeaderSettingRequestCallback(headers);
+        final ResponseResultErrorHandler errorHandler = new ResponseResultErrorHandler();
+
+        if (restTemplate == null) {
+            restTemplate = new RestTemplate();
+        }
+
+        restTemplate.setErrorHandler(errorHandler);
+        restTemplate
+                .execute("http://localhost:8080/start", HttpMethod.POST, requestCallback, response -> {
+                    if (errorHandler.hadError) {
+                        return (errorHandler.getResults());
+                    } else {
+                        return (new ResponseResults(response));
+                    }
+                });
+    }
+
+    private class ResponseResultErrorHandler implements ResponseErrorHandler {
+        private ResponseResults results = null;
+        private Boolean hadError = false;
+
+        private ResponseResults getResults() {
+            return results;
+        }
+
+        @Override
+        public boolean hasError(ClientHttpResponse response) throws IOException {
+            hadError = response.getRawStatusCode() >= 400;
+            return hadError;
+        }
+
+        @Override
+        public void handleError(ClientHttpResponse response) throws IOException {
+            results = new ResponseResults(response);
+        }
+    }
+}
